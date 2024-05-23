74fe686696bd8d2f7d72732678533d56887d30f9
sahilaherwar <57956156+sahilaherwar@users.noreply.github.com>
2024-05-23 16:47:17 +0530

    Update Nav.js

--- Nav.js
+++ Nav.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const navbar = document.getElementById("navbar");
   
     window.addEventListener("scroll", function() {
-      if (window.scrollY > 100) {
+      if (window.scrollY > 120) {
         navbar.classList.add("nav_black");
       } else {
         navbar.classList.remove("nav_black");
